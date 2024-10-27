f7b64cff071ca54a5b1dd6fde11023ae3b37b901
Nabiur siddique <nabiursiddique01@gmail.com>
2024-10-27 14:49:49 +0600

    After decreasing the quantity now card will be deleted

--- src/App.jsx
+++ src/App.jsx
@@ -38,13 +38,12 @@ function App() {
     setCartProducts(
       cartProducts
         .map((item) =>
-          item.id === id && item.quantity > 1
-            ? { ...item, quantity: item.quantity - 1 }
-            : item
+          item.id === id ? { ...item, quantity: item.quantity - 1 } : item
         )
         .filter((item) => item.quantity > 0)
     );
   };
+
 
   useEffect(() => {
     let total = 0;
